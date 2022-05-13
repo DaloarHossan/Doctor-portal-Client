1fd590257b192ebaa79931e13b44cb27b1e278a4
Daloar Hossan <daloarhossan128@gmail.com>
2022-05-13 22:30:45 +0600

    login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import AppointmentPage from './Pages/Appointment/AppointmentPage';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Signup from './Pages/Login/Signup';
 import Navbar from './Pages/Sheared/Navbar/Navbar';
 
 function App() {
@@ -10,7 +12,9 @@ function App() {
        <Navbar></Navbar>
        <Routes>
          <Route path="/" element={<Home></Home>}></Route>
-         <Route path="/appointment" element={<AppointmentPage></AppointmentPage>}></Route>
+         <Route path="login" element={<Login></Login>}></Route>
+         <Route path="signup" element={<Signup></Signup>}></Route>
+         <Route path="appointment" element={<AppointmentPage></AppointmentPage>}></Route>
        </Routes>
     </div>
   );
---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from "../../firebase.config";
+import Loading from "../Sheared/Loading";
+
+const Login = () => {
+  const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+  const navigate=useNavigate(auth);
+  let setError;
+	const { register, handleSubmit, formState: { errors } } = useForm();
+  if(loading){
+    return <Loading></Loading>
+  }
+  if(error){
+     setError =error.message;
+  }
+  if(user){
+    navigate('/')
+  }
+	const onSubmit = data => {
+    signInWithEmailAndPassword(data.email, data.password)
+  };
+  const handelGoogle=()=>{
+    signInWithGoogle()
+  }
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="card w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="text-center text-xl">Login</h2>
+          <form onSubmit={handleSubmit(onSubmit)} >
+			  <div className="form-control w-full max-w-xs">
+			  <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="Email"
+              className="input input-bordered w-full max-w-xs"
+			  {...register("email", { required:{value:true, message: 'Email is Required'} ,
+				pattern: {
+					value:  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+					message: 'Please input a valid email' 
+				  }})}
+            />
+            <label className="label">
+			{errors.email?.type === 'required' &&  <small className="label-text text-red-500">{errors.email.message}</small>}
+			{errors.email?.type === 'pattern' &&  <small className="label-text text-red-500">{errors.email.message}</small>}
+            </label>
+			  </div>
+            <div>
+			<label className="label">
+              <span className="label-text">Password</span>
+            </label>
+			</div>
+            
+            <input
+              type="password"
+              placeholder="Password"
+              className="input input-bordered w-full max-w-xs"
+			  {...register("password", { required:{value:true, message: 'Password is Required'},
+				pattern: {
+					value:/^(?=.*\d).{6,20}$/,
+					message: 'Must be at least 6 characters' 
+			}})}
+            />
+            <label className="label">
+			{errors.password?.type === 'required' &&  <small className="label-text text-red-500">{errors.password.message}</small>}
+			{errors.password?.type === 'pattern' &&  <small className="label-text text-red-500">{errors.password.message}</small>}
+            </label>
+            <p><small className="label-text text-red-500">{setError}</small></p>
+			<input type="submit" className="btn w-full max-w-xs text-white" value="Login"/>
+          </form>
+		  <p><small>New to Doctors Portal? <Link className="text-primary" to="/singup">Create New Account</Link></small></p>
+		  <div className="divider">OR</div>
+		  <button onClick={handelGoogle} className="btn btn-outline">Continue With Google</button>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
